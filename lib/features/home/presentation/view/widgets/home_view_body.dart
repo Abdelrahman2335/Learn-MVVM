d7f714ffbaf9eab5c6a7bb0e5be1ceffac0dc7ba
Abdelrahman2335 <125411627+Abdelrahman2335@users.noreply.github.com>
2025-07-04 06:44:17 +0300

    add Google Fonts

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,13 +1,26 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/view/widgets/custom_app_bar.dart';
-import 'package:bookly/features/home/presentation/view/widgets/custom_book.dart';
 import 'package:bookly/features/home/presentation/view/widgets/top_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Column(children: const [CustomAppBar(), TopBooksListView()]);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        CustomAppBar(),
+        Padding(
+          padding: EdgeInsets.only(right: 18),
+          child: TopBooksListView(),
+        ),
+         SizedBox(height: 50),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18),
+          child: Text("Best Sellers", style: Styles.titleMediam),
+        ),
+      ],
+    );
   }
 }
